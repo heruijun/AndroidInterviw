0969382d2fe7218039c6cd45978f8c6421301009
heruijun66 <13585546884@163.com>
2018-02-28 22:50:06 +0800

    一些修改

--- App/src/main/java/interview/heruijun/com/androidinterview/activity/MainActivity.java
+++ App/src/main/java/interview/heruijun/com/androidinterview/activity/MainActivity.java
@@ -1,28 +1,32 @@
 package interview.heruijun.com.androidinterview.activity;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.heruijun.baselibrary.activity.BaseActivity;
 import com.heruijun.baselibrary.config.RouterPath;
+import com.heruijun.baselibrary.recycler.DividerItemDecoration;
+import com.heruijun.baselibrary.recycler.OnBottomReachedListener;
+import com.heruijun.baselibrary.recycler.OnRecylerItemClick;
 import com.heruijun.baselibrary.util.Utils;
 
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
 import interview.heruijun.com.androidinterview.R;
 import interview.heruijun.com.androidinterview.classloader.ClassloaderActivity;
 import interview.heruijun.com.androidinterview.normalwebview.NormalwebviewActivity;
@@ -33,9 +37,10 @@ import interview.heruijun.com.androidinterview.webview.WebviewActivity;
  * Created by heruijun on 2018/1/5.
  */
 
-public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends BaseActivity {
 
     private static final String TAG = "MainActivity";
+
     private static final String LINE_0 = "Activity相关";
     private static final String LINE_1 = "普通webview";
     private static final String LINE_2 = "登陆注册";
@@ -49,16 +54,27 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
     private static final String LINE_10 = "service用法";
     private static final String LINE_11 = "插件化与热修复";
 
-    private List<String> lines = Arrays.asList(LINE_0, LINE_1, LINE_2, LINE_3, LINE_4, LINE_5, LINE_6, LINE_7, LINE_8, LINE_9, LINE_10, LINE_11);
+    private List<String> lines = Arrays.asList(
+            LINE_0, LINE_1, LINE_2, LINE_3, LINE_4, LINE_5,
+            LINE_6, LINE_7, LINE_8, LINE_9, LINE_10, LINE_11
+    );
+
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+    MyRecyclerAdapter adapter;
 
     private MyHandler myHandler = new MyHandler(this);
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        GodEye.instance().installAll(getApplication(), new CrashFileProvider(this, new Serializer() {
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        //        GodEye.instance().installAll(getApplication(), new CrashFileProvider(this, new Serializer() {
 //            @Override
 //            public String serialize(Object o) {
 //                return GsonUtil.toJson(o);
@@ -71,9 +87,7 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
 //        }));
 //        GodEyeMonitor.work(this);
 //        StartupTracer.get().onSplashCreate();
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, lines);
-        setListAdapter(adapter);
-        this.getListView().setOnItemClickListener(this);
+
         // ActivityMgr.getInstance().addActivity(this);     // 模拟内存泄漏
 
         Uri uri = getIntent().getData();
@@ -104,57 +118,73 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
             Log.e(TAG, "name: " + name);
         }
         // ARouter.getInstance().build(uri).navigation();
+
+        adapter = new MyRecyclerAdapter(lines);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnRecylerItemClick(new OnItemClick());
+
+        adapter.setOnBottomReachedListener(new OnBottomReachedListener() {
+            @Override
+            public void onBottomReached(int position) {
+                Toast.makeText(MainActivity.this, "滚动到底部", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String lineName = lines.get(position);
-        Intent intent = null;
-        switch (lineName) {
-            case LINE_0:
-                // startActivity(new Intent(MainActivity.this, InterviewActivity.class));
-                ARouter.getInstance().build(RouterPath.PATH_ACTIVITY).navigation();
-                break;
-            case LINE_1:
-                startActivity(new Intent(MainActivity.this, NormalwebviewActivity.class));
-                break;
-            case LINE_2:
-                // Toast.makeText(MainActivity.this, Utils.createUUID(), Toast.LENGTH_SHORT).show();    // UUID生成
-                startActivity(new Intent(MainActivity.this, AccountActivity.class));
-                break;
-            case LINE_3:
-                Toast.makeText(MainActivity.this,
-                        Utils.getRunningProcess(MainActivity.this).processName, Toast.LENGTH_SHORT).show();
-                break;
-            case LINE_4:
-                startActivity(new Intent(MainActivity.this, AnimationActivity.class));
-                break;
-            case LINE_5:
-                // myHandler.sendEmptyMessage(0);
-                startActivity(new Intent(MainActivity.this, HandlerExample.class));
-                break;
-            case LINE_6:
-                intent = new Intent(MainActivity.this, WebviewActivity.class);
-                startActivity(intent);
-                break;
-            case LINE_7:
-                for (int i = 0; i < 10000; i++) {
-                    A a = new A();
-                    a.say();
-                }
-                break;
-            case LINE_8:
-                startActivity(new Intent(MainActivity.this, DispatcherExampleActivity.class));
-                break;
-            case LINE_9:
-                startActivity(new Intent(MainActivity.this, DispatcherViewActivity.class));
-                break;
-            case LINE_10:
-                startActivity(new Intent(MainActivity.this, ServiceActivity.class));
-                break;
-            case LINE_11:
-                startActivity(new Intent(MainActivity.this, ClassloaderActivity.class));
-                break;
+    private class OnItemClick implements OnRecylerItemClick {
+
+        @Override
+        public void onItemClick(int position) {
+            String line = lines.get(position);
+            switch (line) {
+                case LINE_0:
+                    // startActivity(new Intent(MainActivity.this, InterviewActivity.class));
+                    ARouter.getInstance().build(RouterPath.PATH_ACTIVITY).navigation();
+                    break;
+                case LINE_1:
+                    startActivity(new Intent(MainActivity.this, NormalwebviewActivity.class));
+                    break;
+                case LINE_2:
+                    // Toast.makeText(MainActivity.this, Utils.createUUID(), Toast.LENGTH_SHORT).show();    // UUID生成
+                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                    break;
+                case LINE_3:
+                    Toast.makeText(MainActivity.this,
+                            Utils.getRunningProcess(MainActivity.this).processName, Toast.LENGTH_SHORT).show();
+                    break;
+                case LINE_4:
+                    startActivity(new Intent(MainActivity.this, AnimationActivity.class));
+                    break;
+                case LINE_5:
+                    // myHandler.sendEmptyMessage(0);
+                    startActivity(new Intent(MainActivity.this, HandlerExample.class));
+                    break;
+                case LINE_6:
+                    startActivity(new Intent(MainActivity.this, WebviewActivity.class));
+                    break;
+                case LINE_7:
+                    for (int i = 0; i < 10000; i++) {
+                        A a = new A();
+                        a.say();
+                    }
+                    break;
+                case LINE_8:
+                    startActivity(new Intent(MainActivity.this, DispatcherExampleActivity.class));
+                    break;
+                case LINE_9:
+                    startActivity(new Intent(MainActivity.this, DispatcherViewActivity.class));
+                    break;
+                case LINE_10:
+                    startActivity(new Intent(MainActivity.this, ServiceActivity.class));
+                    break;
+                case LINE_11:
+                    startActivity(new Intent(MainActivity.this, ClassloaderActivity.class));
+                    break;
+            }
         }
     }
 
@@ -191,4 +221,60 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
 //        GodEyeMonitor.shutDown();
 //        GodEye.instance().uninstallAll();
     }
+
+    private static class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+
+        List<String> lines;
+        OnBottomReachedListener onBottomReachedListener;
+        OnRecylerItemClick onRecylerItemClick;
+
+        public MyRecyclerAdapter(List<String> lines) {
+            this.lines = lines;
+        }
+
+        public void setOnBottomReachedListener(OnBottomReachedListener onBottomReachedListener) {
+            this.onBottomReachedListener = onBottomReachedListener;
+        }
+
+        public void setOnRecylerItemClick(OnRecylerItemClick onRecylerItemClick) {
+            this.onRecylerItemClick = onRecylerItemClick;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup root, int viewType) {
+            View view = LayoutInflater.from(root.getContext()).inflate(R.layout.list_item, root, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            if (position == lines.size() - 1 && onBottomReachedListener != null) {
+                onBottomReachedListener.onBottomReached(position);
+            }
+            holder.textView.setText(lines.get(position));
+            if (onRecylerItemClick != null) {
+                holder.textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onRecylerItemClick.onItemClick(position);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return lines.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView textView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.text_title);
+            }
+        }
+    }
+
 }
